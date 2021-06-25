b5f4f0487046ecce2fbaf3a8e9ad7e8198be6eca
phiphamdnvn <phiphamdnvn@gmail.com>
2021-06-25 12:08:45 +0700

    Define field in MainActivity.java

--- app/src/main/java/com/phipham/notificationscheduler/MainActivity.java
+++ app/src/main/java/com/phipham/notificationscheduler/MainActivity.java
@@ -2,13 +2,25 @@ package com.phipham.notificationscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobScheduler;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int JOB_ID = 0;
+    private JobScheduler mScheduler;
 
+    // Switches for setting job options.
+    private Switch mDeviceIdleSwitch;
+    private Switch mDeviceChargingSwitch;
+
+    // Override deadline seekbar.
+    private SeekBar mSeekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
